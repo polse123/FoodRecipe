db530dc8106c8c2c33185d0cfb1efb5e56342851
polse123 <sorenbendtsen1993@gmail.com>
2018-03-08 10:56:02 +0100

    s

---
+++ FoodRecipe/src/foodrecipe/FoodRecipe.java
@@ -0,0 +1,16 @@
+package foodrecipe;
+
+/**
+ *
+ * @author Søren Bendtsen
+ */
+public class FoodRecipe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code applicastion logic here
+    }
+    
+}
