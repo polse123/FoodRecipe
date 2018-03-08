5817c6c4b3a5900ef883c43b8266e04f292bbae9
polse123 <sorenbendtsen1993@gmail.com>
2018-03-08 10:56:32 +0100

    s

--- FoodRecipe/src/foodrecipe/FoodRecipe.java
+++ FoodRecipe/src/foodrecipe/FoodRecipe.java
@@ -10,7 +10,7 @@ public class FoodRecipe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code applicastion logic here
+        // TODO code applicastiteston logic here
     }
     
 }
