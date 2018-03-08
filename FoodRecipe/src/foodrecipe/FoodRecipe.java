5c54315b88f5d4db6bd3f89f1d737fb243b9e9de
niko6116 <niko6116@github.com>
2018-03-08 11:13:36 +0100

    Nikolajs test
    test

--- FoodRecipe/src/foodrecipe/FoodRecipe.java
+++ FoodRecipe/src/foodrecipe/FoodRecipe.java
@@ -12,6 +12,7 @@ public class FoodRecipe {
     public static void main(String[] args) {
         // TODO code applicastiteston logic here
        // frederikstest
+       // nikolajstest
     }
     
 }
