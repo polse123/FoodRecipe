3ceeb5670f9f1157adae985ef794114755174439
polse123 <sorenbendtsen1993@gmail.com>
2018-03-08 12:22:29 +0100

    Set
    ændret recipe

--- FoodRecipe/src/business/Recipe.java
+++ FoodRecipe/src/business/Recipe.java
@@ -5,12 +5,14 @@
  */
 package business;
 
+import java.util.Set;
+
 /**
  *
  * @author Frederik
  */
-public class Recipe
-{
+public class Recipe {
+
     private String name;
 
     // Forret, hovedret,dessert, snack, kaffe ....
@@ -22,92 +24,89 @@ public class Recipe
     private String procedure;
 
     //Ingredienser
-    private String ingredients;
+    private Set<String> ingredients;
 
     //Billede af det færdige resutat:
     private String pictureFile;
 
     // Retten passer til..
-    private Recipe matches;
+    private Set<Recipe> matches;
 
     //Tilbehør
-    private String accessories;
+    private Set<String> accessories;
 
-    public Recipe(String name, int time, String procedure, String pictureFile)
-    {
+    public Recipe(String name, int time, String procedure, Set<String> ingredients) {
         this.name = name;
         this.time = time;
         this.procedure = procedure;
-        this.pictureFile = pictureFile;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public int getTime()
-    {
-        return time;
-    }
-
-    public void setTime(int time)
-    {
-        this.time = time;
-    }
-
-    public String getProcedure()
-    {
-        return procedure;
-    }
-
-    public void setProcedure(String procedure)
-    {
-        this.procedure = procedure;
-    }
-
-    public String getPictureFile()
-    {
-        return pictureFile;
-    }
-
-    public void setPictureFile(String pictureFile)
-    {
-        this.pictureFile = pictureFile;
-    }
-
-    public Recipe getMatches()
-    {
-        return matches;
-    }
-
-    public void setMatches(Recipe matches)
-    {
-        this.matches = matches;
-    }
-
-    public String getIngredients()
-    {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients)
-    {
         this.ingredients = ingredients;
     }
 
-    public String getAccessories()
-    {
+    public Recipe(String name, int time, String procedure,
+           Set<String> ingredients, String pictureFile, Set<Recipe> matches, 
+           Set<String> accessories) {
+        this.name = name;
+        this.time = time;
+        this.procedure = procedure;
+        this.ingredients = ingredients;
+        this.pictureFile = pictureFile;
+        this.matches = matches;
+        this.accessories = accessories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(String procedure) {
+        this.procedure = procedure;
+    }
+
+    public String getPictureFile() {
+        return pictureFile;
+    }
+
+    public void setPictureFile(String pictureFile) {
+        this.pictureFile = pictureFile;
+    }
+
+    public Set<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Recipe> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(Set<Recipe> matches) {
+        this.matches = matches;
+    }
+
+    public Set<String> getAccessories() {
         return accessories;
     }
 
-    public void setAccessories(String accessories)
-    {
+    public void setAccessories(Set<String> accessories) {
         this.accessories = accessories;
     }
 
